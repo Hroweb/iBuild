ee8b6905f810fa613132d49e73b12ac0ee8af28d
Hroweb <hroweb@HroWebs-MacBook-Pro.local>
2023-07-13 00:26:41 +0400

    initial commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Main from './Main'
+import '../css/app.css'
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+)
